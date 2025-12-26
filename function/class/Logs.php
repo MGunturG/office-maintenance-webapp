9a8fd7003bd19b9ec77e2787382b8a5c45133fa8
MGunturG <39583921+MGunturG@users.noreply.github.com>
2025-12-26 14:30:00 +0700

    add logs class and log table on sql

---
+++ function/class/Logs.php
@@ -0,0 +1,10 @@
+<?php
+
+class Logs {
+	function LogCreate($entity_type, $entity_id, $action, $by_user_id) {
+		run_query(
+			"INSERT INTO activity_log (activity_log_entity_type, activity_log_entity_id, activity_log_action, activity_log_user_id) VALUES ('$entity_type', '$entity_id', '$action', '$by_user_id')"
+		);
+	}
+}
+?>
