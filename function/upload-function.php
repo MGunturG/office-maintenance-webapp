10e657609219ea8bcb14079360bcf33fe5e15a39
MGunturG <39583921+MGunturG@users.noreply.github.com>
2025-12-31 11:00:37 +0700

    refactor code

--- function/upload-function.php
+++ function/upload-function.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+// alert will always be showed
+$_SESSION['alert_value'] = "show";
+$_SESSION['alert_button_text'] = "OK";
+
 require '../config.php';
 require 'db-query.php';
 include_once 'class/Items.php';
@@ -16,135 +20,117 @@ $_Ticket = new Tickets;
 // reference code
 // https://www.w3schools.com/php/php_file_upload.asp
 
-$target_dir = "../assets/uploads/images/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$large_file = 0;
-$bad_format = 0;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// upload function configuration
+$target_directory = "../assets/uploads/images/";
+$upload_filename = $target_directory . basename($_FILES['fileToUpload']["name"]);
+$upload_filetype = strtolower(pathinfo($upload_filename, PATHINFO_EXTENSION));
 
-$upload_action_type = $_POST['action'];
-$sql_id_query = $_POST['id']; // or item id
+// upload flags
+$can_upload = true;
+$large_file = false;
+$bad_file = false;
+$duplicate_file = false;
+
+// check upload type
+// 1. upload for comment; or
+// 2. upload for change item's picture
+$upload_type = $_POST['action'];
+$item_id = $_POST['id'];
 $ticket_id = $_POST['ticket_id'];
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["upload_Submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    // echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
+// check if image uploaded is an actual image
+if (isset($_POST['upload_Submit'])) {
+	$check_image = getimagesize($_FILES['fileToUpload']['tmp_name']);
+
+	// if the uploaded file is an actual image
+	// keep $can_upload as true, but check
+	// the file size and filetype/extension
+	if ($check_image == true) {
+
+		// check file duplicate
+		if (file_exists($upload_filename)) {
+			$duplicate_file = true;
+			$can_upload = false;
+		}
+
+		// check image file size
+		// if over 5mb, dont upload
+		if ($_FILES['fileToUpload']['size'] > 5000000) {
+			$large_file = true;
+			$can_upload = false;
+		}
+
+		// check image file extension
+		// only accept jpg, jpeg, and png
+		$allowed_filetype = ['jpg', 'jpeg', 'png'];
+		if (!in_array($upload_filetype, $allowed_filetype)) {
+			$bad_file = true;
+			$can_upload = false;
+		} 
+
+	} else {
+		$can_upload = false;
+		$_SESSION['alert_text'] = "Format file tidak didukung!";
+	}
 }
 
-// Check if file already exists
-// if exists, delete. upload new one
 
-// update 17-dec-2025
-// if file exists, throw error
-if (file_exists($target_file)) {
-  // unlink($target_file); ==> to delete file
-  $uploadOk = 0;
-  $duplicate_file = 1;
+if ($can_upload == true) {
+	
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $upload_filename)) {
+		// create alert message
+		$_SESSION['alert_title'] = "Mantap!";
+		$_SESSION['alert_text'] = "Gambar terupload!";
+		$_SESSION['alert_icon'] = "success";
+
+		// if uploading to change item's picture
+		if ($upload_type == "insert_item_picture") {
+			$_Item->ItemChangePicture($item_id, BASE_URL."/assets/uploads/images/".basename( $_FILES["fileToUpload"]["name"]));
+			header("location:".BASE_URL."/dashboard/item/item-detail.php?id=".$item_id);
+
+		// if uploading for ticket comment
+		} elseif ($upload_type == "insert_comment_picture") {
+			$image_path = BASE_URL."/assets/uploads/images/".basename( $_FILES["fileToUpload"]["name"]);
+			$_Ticket->TicketAddComment(
+		        $ticket_id,
+		        '<img src="' . $image_path . '" class="card-img-top img-fluid" style="max-width: 50%; height: auto">',
+		        $_SESSION['user_uname'] 
+			);
+			header("location:".BASE_URL."/dashboard/ticket/ticket-detail.php?id=".$ticket_id);
+		}
+
+	// here if move_uploaded_file returned false
+	// or error moving the file
+	} else {
+		// error message can't move file
+		$_SESSION['alert_title'] = "Oops...";
+		$_SESSION['alert_text'] = "Host Error: Can't move uploaded file. Permission issue or destination doesn't exists!";
+		$_SESSION['alert_icon'] = "error";
+	}
+
+} else { // error uploading file
+	// create alert error message
+	$_SESSION['alert_title'] = "Oops...";
+	$_SESSION['alert_icon'] = "error";
+
+	if ($duplicate_file) {
+		$_SESSION['alert_text'] = "File duplikat, upload dengan gambar yang lain ya";
+	}
+
+	if ($large_file) {
+		$_SESSION['alert_text'] = "Ukuran file terlalu besar! Max 5MB.";
+	}
+
+	if ($bad_file) {
+		$_SESSION['alert_text'] = "Format file tidak didukung!";
+	}
+
+	// redirect based on upload action type
+	if ($upload_type == "insert_item_picture") {
+		header("location:".BASE_URL."/dashboard/item/item-detail.php?id=".$item_id);
+	} elseif ($upload_type == "insert_comment_picture") {
+		header("location:".BASE_URL."/dashboard/ticket/ticket-detail.php?id=".$ticket_id);
+	}
 }
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 5000000) { // in bytes, limit to 5000KB or 5MB
-  // echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-  $large_file = 1;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-  // echo "Sorry, only JPG, JPEG & PNG files are allowed.";
-  $uploadOk = 0;
-  $bad_format = 1;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-
-  if ($large_file == 1) {
-    // alert session file too large
-    $_SESSION['alert_value'] = "show"; // put any value, if null, alert not showing
-    $_SESSION['alert_title'] = "Oops...";
-    $_SESSION['alert_text'] = "Ukuran file terlalu besar";
-    $_SESSION['alert_icon'] = "error"; // success, question, error, warning, info
-    $_SESSION['alert_button_text'] = "OK";
-  } elseif ($duplicate_file == 1) {
-    // alert session bad file format
-    $_SESSION['alert_value'] = "show"; // put any value, if null, alert not showing
-    $_SESSION['alert_title'] = "Oops...";
-    $_SESSION['alert_text'] = "File sudah ada, mohon input file dengan nama yang unik";
-    $_SESSION['alert_icon'] = "error"; // success, question, error, warning, info
-    $_SESSION['alert_button_text'] = "OK";
-  } elseif ($bad_format == 1) {
-    // alert session bad file format
-    $_SESSION['alert_value'] = "show"; // put any value, if null, alert not showing
-    $_SESSION['alert_title'] = "Oops...";
-    $_SESSION['alert_text'] = "Format file tidak didukung";
-    $_SESSION['alert_icon'] = "error"; // success, question, error, warning, info
-    $_SESSION['alert_button_text'] = "OK";
-  } else {
-    $_SESSION['alert_value'] = "show"; // put any value, if null, alert not showing
-    $_SESSION['alert_title'] = "Oops...";
-    $_SESSION['alert_text'] = "Unknown Error : 0x0001";
-    $_SESSION['alert_icon'] = "error"; // success, question, error, warning, info
-    $_SESSION['alert_button_text'] = "OK";
-  }
-
-  // check upload action type
-  if ($upload_action_type == "insert_item_picture") {
-    header("location:".BASE_URL."/dashboard/item/item-detail.php?id=".$sql_id_query);
-  } elseif ($upload_action_type == "insert_comment_picture") {
-    header("location:".BASE_URL."/dashboard/ticket/ticket-detail.php?id=".$ticket_id);
-  }
-
-
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    // check upload action type
-    if ($upload_action_type == "insert_item_picture") {
-      $_Item->ItemChangePicture($sql_id_query, BASE_URL."/assets/uploads/images/".basename( $_FILES["fileToUpload"]["name"]));
-      
-      $_SESSION['alert_value'] = "show"; // put any value, if null, alert not showing
-      $_SESSION['alert_title'] = "Mantap!";
-      $_SESSION['alert_text'] = "Gambar berhasil diperbarui";
-      $_SESSION['alert_icon'] = "success"; // success, question, error, warning, info
-      $_SESSION['alert_button_text'] = "OK";
-
-      header("location:".BASE_URL."/dashboard/item/item-detail.php?id=".$sql_id_query);
-
-    } elseif ($upload_action_type == "insert_comment_picture") {
-      $image_path = BASE_URL."/assets/uploads/images/".basename( $_FILES["fileToUpload"]["name"]);
-
-
-      $_Ticket->TicketAddComment(
-        $ticket_id,
-        '<img src="' . $image_path . '" class="card-img-top img-fluid" style="max-width: 50%; height: auto">',
-        $_SESSION['user_uname'] 
-      );
-
-      header("location:".BASE_URL."/dashboard/ticket/ticket-detail.php?id=".$ticket_id);
-    }
-
-  } else {
-    $_SESSION['alert_value'] = "show"; // put any value, if null, alert not showing
-    $_SESSION['alert_title'] = "Oops...";
-    $_SESSION['alert_text'] = "Unknown Error : 0x0001";
-    $_SESSION['alert_icon'] = "error"; // success, question, error, warning, info
-    $_SESSION['alert_button_text'] = "OK";
-
-    // check upload action type
-    if ($upload_action_type == "insert_item_picture") {
-      header("location:".BASE_URL."/dashboard/item/item-detail.php?id=".$sql_id_query);
-    } elseif ($upload_action_type == "insert_comment_picture") {
-      header("location:".BASE_URL."/dashboard/ticket/ticket-detail.php?id=".$ticket_id);
-    }
-  }
-}
 ?>
